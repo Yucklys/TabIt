52a280ea3d34f887e1cf0cb3419fee2e4016fabd
Yucklys <yucklys687@outlook.com>
2025-10-22 19:59:10 -0500

    feat: add tab apis

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,25 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { Button } from './components/ui/button'
+import { getTabInfoList, getUngroupedTabs } from './api/tabs';
+
+const fetchTabs = async () => {
+  const tabs = await getUngroupedTabs();
+  console.log('Found tabs:', tabs.length);
+  
+  const tabInfoList = getTabInfoList(tabs);
+  console.log('Valid tabs:', tabInfoList.length);
+  
+  tabInfoList.forEach((tabInfo) => {
+    console.log(`Tab ${tabInfo.index}:`, tabInfo);
+  });
+};
 
 function App() {
   const [count, setCount] = useState(0)
+
+  useEffect(() => {
+    fetchTabs();
+  }, [count]);
 
   return (
     <>
